38838e93deef3876084f99a6e72f898a15b1470d
Emanuel Eriksson <emanuel.eriksson@gmail.com>
2014-01-02 20:51:26 +0100

    added grunt task for example

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,9 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-open');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         build_dir: 'build',
@@ -76,9 +79,25 @@ module.exports = function(grunt) {
             dev: {
                 reporters: 'dots'
             }
+        },
+        connect: {
+            example:{
+                options: {
+                    port:3000
+                }
+            }
+        },
+        open: {
+            example:{
+                path: 'http://localhost:3000/example/index.html'
+            }
+        },
+        watch:{
+
         }
 
     });
     grunt.registerTask('build', ['clean','jshint','karma:continuous','concat','uglify']);
     grunt.registerTask('default', ['build']);
+    grunt.registerTask('example',['build','connect:example','open:example','watch']);
 };
