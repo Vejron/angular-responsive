23aabcdb228252c4c4272d9ca7fc0698ea5ec360
Emanuel Eriksson <emanuel.eriksson@komatsuforest.com>
2014-01-02 11:48:42 +0100

    moving to includes to prepare for custom widts

--- example/ExampleController.js
+++ example/ExampleController.js
@@ -3,7 +3,8 @@ angular.module( 'example', ['responsive.listener','responsive.responder.rule','r
 
 
 .controller( 'ExampleController', ['$scope','eeEventListener','options','$window',function( $scope,listener,options,$window ){
-        $scope.classes = ["visible-xs","visible-sm","visible-md","visible-lg","hidden-xs","hidden-sm","hidden-md","hidden-lg"];
+        $scope.visibleClasses = ["visible-xs","visible-sm","visible-md","visible-lg"];
+        $scope.hiddenClasses = ["hidden-xs","hidden-sm","hidden-md","hidden-lg"];
         $scope.width = $window.innerWidth;
         angular.element($window).on('resize', function(event){
             $scope.width = $window.innerWidth;
