2f8d7df65d09253772fa29ccf4331505138317ce
Emanuel Eriksson <emanuel.eriksson@komatsuforest.com>
2014-01-02 08:06:24 +0100

    update to example page

--- example/ExampleController.js
+++ example/ExampleController.js
@@ -2,7 +2,11 @@ angular.module( 'example', ['responsive.listener','responsive.responder.rule','r
 ])
 
 
-.controller( 'ExampleController', ['$scope','eeEventListener','options',function( $scope,listener,options ){
-    $scope.test = "TEST!";
-    $scope.bbb = true;
+.controller( 'ExampleController', ['$scope','eeEventListener','options','$window',function( $scope,listener,options,$window ){
+        $scope.classes = ["visible-xs","visible-sm","visible-md","visible-lg","hidden-xs","hidden-sm","hidden-md","hidden-lg"];
+        $scope.width = $window.innerWidth;
+        angular.element($window).on('resize', function(event){
+            $scope.width = $window.innerWidth;
+            $scope.$apply();
+        });
 }]);
