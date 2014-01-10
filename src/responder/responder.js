45a41d069679cd6c1c1742f04da242200d4e5bd7
Emanuel Eriksson <emanuel.eriksson@gmail.com>
2014-01-10 21:53:05 +0100

    created BooleanResponder

--- src/responder/responder.js
+++ src/responder/responder.js
@@ -1,24 +1,32 @@
 angular.module( 'responsive.responder', ['responsive.listener','responsive.responder.rule'])
 .factory('responderFactory',['widthEventListener','responderRuleFactory',function(listener,responderRuleFactory){
-    var Responder = function(rule){
-        this.rule = rule;
+    var Responder = function(){
+        this.listener = null;
     };
+    Responder.prototype.createListener = function(func){};
     Responder.prototype.registerTrigger = function(func){
-        var rule = this.rule;
-        this.listener = function(width){
-            var response = rule.widthValue(width.name);
-            func(response);
-        };
+        this.createListener(func);
         listener.subscribe(this.listener);
     };
     Responder.prototype.deregister = function(){
         listener.unSubscribe(this.listener);
     };
+    var BooleanResponder = function(rule){
+        this.rule = rule;
+    };
+    BooleanResponder.prototype = new Responder();
+    BooleanResponder.prototype.createListener = function(func){
+        var rule = this.rule;
+        this.listener = function(width){
+            var response = rule.widthValue(width.name);
+            func(response);
+        };
+    };
     var ResponderFactory = function(){};
     ResponderFactory.prototype.getBooleanResponder = function(classes){
         var rule = responderRuleFactory.getRule(classes);
 
-        return new Responder(rule);
+        return new BooleanResponder(rule);
     };
     return new ResponderFactory();
 }]);
