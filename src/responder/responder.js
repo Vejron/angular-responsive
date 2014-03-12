0b0b692f11cc9b726036e52eef1bd65c2b71d47c
Emanuel Eriksson <emanuel.eriksson@gmail.com>
2014-03-12 14:40:14 +0100

    added string responder

--- src/responder/responder.js
+++ src/responder/responder.js
@@ -1,32 +1,59 @@
 angular.module( 'responsive.responder', ['responsive.listener','responsive.responder.rule'])
-.factory('responderFactory',['widthEventListener','responderRuleFactory',function(listener,responderRuleFactory){
-    var Responder = function(){
-        this.listener = null;
-    };
-    Responder.prototype.createListener = function(func){};
-    Responder.prototype.registerTrigger = function(func){
-        this.createListener(func);
-        listener.subscribe(this.listener);
-    };
-    Responder.prototype.deregister = function(){
-        listener.unSubscribe(this.listener);
-    };
-    var BooleanResponder = function(rule){
-        this.rule = rule;
-    };
-    BooleanResponder.prototype = new Responder();
-    BooleanResponder.prototype.createListener = function(func){
-        var rule = this.rule;
-        this.listener = function(width){
-            var response = rule.widthValue(width.name);
-            func(response);
+.factory('responderFactory',['widthEventListener','responderRuleFactory',function(eventListener,responderRuleFactory){
+        var ResponderFactory = function(){};
+        var Responder = function(){
+            this.listener = null;
         };
-    };
-    var ResponderFactory = function(){};
-    ResponderFactory.prototype.getBooleanResponder = function(classes){
-        var rule = responderRuleFactory.getRule(classes);
+        Responder.prototype.createListener = function(func){};
+        Responder.prototype.registerTrigger = function(func){
+            this.createListener(func);
+            eventListener.subscribe(this.listener);
+        };
+        Responder.prototype.deregister = function(){
+            eventListener.unSubscribe(this.listener);
+        };
+        var BooleanResponder = function(rule){
+            this.rule = rule;
+        };
+        BooleanResponder.prototype = new Responder();
+        BooleanResponder.prototype.createListener = function(func){
+            var rule = this.rule;
+            this.listener = function(width){
+                var response = rule.widthValue(width.name);
+                func(response);
+            };
+        };
+        ResponderFactory.prototype.getBooleanResponder = function(classes){
+            var rule = responderRuleFactory.getRule(classes);
 
-        return new BooleanResponder(rule);
-    };
-    return new ResponderFactory();
+            return new BooleanResponder(rule);
+        };
+        var StringResponder = function(ruleSets){
+            this.ruleSets = ruleSets;
+        };
+        StringResponder.prototype = new Responder();
+        StringResponder.prototype.createListener = function(func){
+            var ruleSets = this.ruleSets;
+            this.listener = function(width){
+                for (var i = 0; i < ruleSets.length; i++) {
+                    var ruleSet = ruleSets[i];
+                    if(ruleSet.rule.widthValue(width.name)){
+                        func(ruleSet.response);
+                        return;
+                    }
+                }
+                func(null);
+            };
+        };
+        ResponderFactory.prototype.getStringResponder = function(ruleList){
+            var ruleSets = [];
+            for (var i = 0; i < ruleList.length; i++) {
+                var ruleItem = ruleList[i];
+                var rule = responderRuleFactory.getRule(ruleItem.classes);
+                ruleSets.push({rule:rule,response:ruleItem.response});
+            }
+
+            return new StringResponder(ruleSets);
+        };
+        return new ResponderFactory();
 }]);
--- src/responder/responder.spec.js
+++ src/responder/responder.spec.js
@@ -1,5 +1,5 @@
-describe( 'Boolean Responder', function() {
-	var responder,mockListener,MockResponderRuleFactory;
+describe( 'Responders -',function(){
+    var responder,mockListener,MockResponderRuleFactory;
     beforeEach(function(){
         mockListener = {};
         mockListener.subscribe = function(subscriber){mockListener.subscriber = subscriber;};
@@ -8,51 +8,145 @@ describe( 'Boolean Responder', function() {
         spyOn(mockListener,'subscribe').andCallThrough();
 
         spyOn(mockListener,'unSubscribe').andCallThrough();
-
-        MockResponderRuleFactory = {};
-        MockResponderRuleFactory.getRule = function(){
-            var rule = {};
-            rule.widthValue = function(name){
-                if (name === "small"){
-                    return false;
-                }
-                return true;
-            };
-            return rule;
-        };
+    });
+    var doProvide = function(){
         module('responsive.responder',function($provide) {
 
             $provide.value('widthEventListener', mockListener);
             $provide.value('responderRuleFactory', MockResponderRuleFactory);
         });
+    } ;
+    describe( 'Boolean Responder', function() {
 
-        inject(function(responderFactory) {
-            responder = responderFactory.getBooleanResponder("");
+        beforeEach(function(){
+            MockResponderRuleFactory = {};
+            MockResponderRuleFactory.getRule = function(){
+                var rule = {};
+                rule.widthValue = function(name){
+                    if (name === "small"){
+                        return false;
+                    }
+                    return true;
+                };
+                return rule;
+            };
+            doProvide();
+
+            inject(function(responderFactory) {
+                responder = responderFactory.getBooleanResponder(""); //actual rule values are created above in the MockResponderRuleFactory
+            });
+
+        });
+
+        it ('Shouldn\'t have subscribed on init',function(){
+            expect(mockListener.subscribe).not.toHaveBeenCalled();
+        });
+
+        it ('should register for listener when a trigger function is set', function(){
+            var test = function(){};
+            responder.registerTrigger(test);
+            expect(mockListener.subscribe).toHaveBeenCalled();
+        });
+        it ('should get false on small', function(){
+            var trigger = jasmine.createSpy('trigger');
+            responder.registerTrigger(trigger);
+            mockListener.subscriber({name:"small"});
+            expect(trigger).toHaveBeenCalledWith(false);
+
+        });
+        it ('should get true on big', function(){
+            var trigger = jasmine.createSpy('trigger');
+            responder.registerTrigger(trigger);
+            mockListener.subscriber({name:"big"});
+            expect(trigger).toHaveBeenCalledWith(true);
+
+        });
+    });
+    describe('String Responder - ',function(){
+        var firstResponse,secondResponse;
+        beforeEach(function(){
+            MockResponderRuleFactory = {};
+            MockResponderRuleFactory.getRule = function(classes){
+                var rule = {};
+                rule.match = classes;
+                rule.widthValue = function(name){
+                    if (this.match === name){
+                        return true;
+                    }
+                    return false;
+                };
+                return rule;
+            };
+            doProvide();
+            firstResponse = 'firstResponse';
+            secondResponse = 'secondResponse';
+
+        });
+        describe('simple',function(){
+            beforeEach(function(){
+                inject(function(responderFactory) {
+                    responder = responderFactory.getStringResponder([{classes:'small',response:firstResponse},{classes:'big',response:secondResponse}]);
+                });
+            });
+            it ('Shouldn\'t have subscribed on init',function(){
+                expect(mockListener.subscribe).not.toHaveBeenCalled();
+            });
+            it ('should register for listener when a trigger function is set', function(){
+                var test = function(){};
+                responder.registerTrigger(test);
+                expect(mockListener.subscribe).toHaveBeenCalled();
+            });
+            it ('should get correct response on small', function(){
+                var trigger = jasmine.createSpy('trigger');
+                responder.registerTrigger(trigger);
+                mockListener.subscriber({name:"small"});
+                expect(trigger).toHaveBeenCalledWith(firstResponse);
+
+            });
+            it ('should get correct response on big', function(){
+                var trigger = jasmine.createSpy('trigger');
+                responder.registerTrigger(trigger);
+                mockListener.subscriber({name:"big"});
+                expect(trigger).toHaveBeenCalledWith(secondResponse);
+
+            });
+        });
+        describe('not covering all classes',function(){
+            beforeEach(function(){
+                inject(function(responderFactory) {
+                    responder = responderFactory.getStringResponder([{classes:'small',response:firstResponse}]);
+                });
+            });
+            it ('should get correct response on small', function(){
+                var trigger = jasmine.createSpy('trigger');
+                responder.registerTrigger(trigger);
+                mockListener.subscriber({name:"small"});
+                expect(trigger).toHaveBeenCalledWith(firstResponse);
+
+            });
+            it ('should get null on big', function(){
+                var trigger = jasmine.createSpy('trigger');
+                responder.registerTrigger(trigger);
+                mockListener.subscriber({name:"big"});
+                expect(trigger).toHaveBeenCalledWith(null);
+
+            });
+        });
+        describe('rules sharing classes',function(){
+            beforeEach(function(){
+                inject(function(responderFactory) {
+                    responder = responderFactory.getStringResponder([{classes:'small',response:firstResponse},{classes:'small',response:secondResponse}]);
+                });
+            });
+            it ('should get first in list on small and only one call', function(){
+                var trigger = jasmine.createSpy('trigger');
+                responder.registerTrigger(trigger);
+                mockListener.subscriber({name:"small"});
+                expect(trigger).toHaveBeenCalledWith(firstResponse);
+                expect(trigger.callCount).toBe(1);
+            });
         });
 
     });
 
-	it ('Shouldn\'t have subscribed on init',function(){
-		expect(mockListener.subscribe).not.toHaveBeenCalled();
-	});
-
-	it ('should register for listener when a trigger function is set', function(){
-        var test = function(){};
-        responder.registerTrigger(test);
-        expect(mockListener.subscribe).toHaveBeenCalled();
-	});
-    it ('should get false on small', function(){
-        var trigger = jasmine.createSpy('trigger');
-        responder.registerTrigger(trigger);
-        mockListener.subscriber({name:"small"});
-        expect(trigger).toHaveBeenCalledWith(false);
-
-    });
-    it ('should get true on big', function(){
-        var trigger = jasmine.createSpy('trigger');
-        responder.registerTrigger(trigger);
-        mockListener.subscriber({name:"big"});
-        expect(trigger).toHaveBeenCalledWith(true);
-
-    });
 });
